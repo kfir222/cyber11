5c8c1309b12345220f160057403503a1968bbffa
kfir222 <kfirc6408@gmail.com>
2025-07-20 19:54:31 +0300

    project

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("buy")
+num = int(input("Enter a number: "))
+
+if 0 <= num:
+    binary = ""
+    n = num
+    while n > 0:
+        binary = str(n % 2) + binary
+        n = n // 2
+    print(binary)
+else:
+    print("Number out of range.")
